292834789c69c3531380d599480549a77c98eee5
SYWUkenan <251311992@qq.com>
2021-02-26 10:59:24 +0800

    Initial commit

---
+++ src/main/java/com/wuyusheng/boot/Demo/MyArrayList.java
@@ -0,0 +1,8 @@
+package com.wuyusheng.boot.Demo;
+
+/**
+ * @author yswu
+ * @date 2021-02-26 10:56
+ */
+public class MyArrayList {
+}
---
+++ src/main/java/com/wuyusheng/boot/Demo/test.java
@@ -0,0 +1,17 @@
+package com.wuyusheng.boot.Demo;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * @author yswu
+ * @date 2021-02-25 23:41
+ */
+public class test {
+
+
+    public static void main(String[] args) {
+        ArrayList<Object> objects = new ArrayList<>();
+        Vector<Object> objects1 = new Vector<>();
+    }
+}
